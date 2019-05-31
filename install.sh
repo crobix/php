89c986d783de35143364e1c7e6cd17c5dce7207b
Loic AUDU <loic.audu@inextenso.fr>
2019-05-31 09:34:20 +0200

    fix lib ssl

--- install.sh
+++ install.sh
@@ -18,9 +18,9 @@ export ACCEPT_EULA=Y
 apt-get update && apt-get -y upgrade && apt-get install -y multiarch-support mysql-client msodbcsql mssql-tools openjdk-8-jre-headless ca-certificates-java
 
 # Fix SQLSTATE[01000]: [unixODBC][Driver Manager]Can't open lib '/opt/microsoft/msodbcsql/lib64/libmsodbcsql-13.1.so.9.1' : file not found
-wget http://security.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.0.0_1.0.1t-1+deb7u4_amd64.deb
-dpkg -i libssl1.0.0_1.0.1t-1+deb7u4_amd64.deb
-rm libssl1.0.0_1.0.1t-1+deb7u4_amd64.deb
+wget http://security-cdn.debian.org/debian-security/pool/updates/main/o/openssl/libssl1.1_1.1.0j-1~deb9u1_amd64.deb
+dpkg -i libssl1.1_1.1.0j-1~deb9u1_amd64.deb
+rm libssl1.1_1.1.0j-1~deb9u1_amd64.deb
 
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /root/.bash_profile && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /root/.bashrc && chmod +x /root/.bashrc
 /root/.bashrc
