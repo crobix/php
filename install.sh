c3af78161e4a4bcabcb5736c6ae582bde3e07f7a
Loic AUDU <loic.audu@inextenso.fr>
2019-12-02 14:29:01 +0100

    update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -xe
+#!/bin/sh -ex
 ##
 # Loïc AUDU PHP 7.4 container
 ##
@@ -11,10 +11,10 @@ echo "deb https://packages.sury.org/php/ buster main" > /etc/apt/sources.list.d/
 # Blackfire
 wget -q -O - https://packages.blackfire.io/gpg.key | sudo apt-key add -
 echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
-apt-key update
+
 # PHP
-apt-get update && apt-get upgrade -y && apt-get -y install php7.4-dev php7.4-cli php7.4-bcmath php7.4-curl php-pear php7.4-gd php7.4-mbstring php7.4-mysql php7.4-sqlite3 php7.4-xmlrpc php7.4-xsl php7.4-ldap php7.4-gmp php7.4-intl php7.4-zip php7.4-soap php7.4-xml php7.4-common php7.4-json php7.4-opcache php7.4-readline
-# Disabled : blackfire-agent blackfire-php
+apt-get update && apt-get upgrade -y
+apt-get -y install php7.4-dev php7.4-cli php7.4-bcmath php7.4-curl php-pear php7.4-gd php7.4-mbstring php7.4-mysql php7.4-sqlite3 php7.4-xmlrpc php7.4-xsl php7.4-ldap php7.4-gmp php7.4-intl php7.4-zip php7.4-soap php7.4-xml php7.4-common php7.4-json php7.4-opcache php7.4-readline
 # Disabled ext from repos :  php7.4-imagick php7.4-xdebug php7.4-apcu
 # Disable dependencies : libmagickwand-6.q16-dev
 # Disabled php-redis
@@ -26,9 +26,10 @@ sed -i 's/disable_functions\ \=\ pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait
 
 pecl channel-update pecl.php.net
 ## Fix Tar Error
-wget https://github.com/pear/Archive_Tar/releases/download/1.4.3/Archive_Tar-1.4.3.tgz
-tar -xvf Archive_Tar-1.4.3.tgz
-cp Archive_Tar-1.4.3/Archive/Tar.php /usr/share/php/Archive/Tar.php
+#wget https://github.com/pear/Archive_Tar/releases/download/1.4.3/Archive_Tar-1.4.3.tgz
+#tar -xvf Archive_Tar-1.4.3.tgz
+#cp Archive_Tar-1.4.3/Archive/Tar.php /usr/share/php/Archive/Tar.php
+#rm -rf  Archive_Tar-1.4.3
 
 pecl install sqlsrv-5.7.0preview
 pecl install pdo_sqlsrv-5.7.0preview
@@ -41,22 +42,18 @@ pear install pecl/amqp
 echo "extension=amqp" > /etc/php/7.4/mods-available/amqp.ini
 
 
-pear install pecl/redis-5.0.2
+pear install pecl/redis-5.1.1
 echo "extension=redis" > /etc/php/7.4/mods-available/redis.ini
 
-apt install -y libmagickwand-dev libmagickcore-dev libmagickwand-6.q16-6 libmagickcore-6.q16-6
+apt-get install -y libmagickwand-dev libmagickcore-dev libmagickwand-6.q16-6 libmagickcore-6.q16-6
 pear install pecl/imagick
 echo "extension=imagick" > /etc/php/7.4/mods-available/imagick.ini
 
-#git clone git://github.com/xdebug/xdebug.git
-#cd xdebug
-#git co 2.6.0beta1
-#/usr/bin/phpize7.4 && ./configure --enable-xdebug && make && make install
-pear install pecl/xdebug-2.8.0beta2
+
+pear install pecl/xdebug-2.8.1
 echo "zend_extension=xdebug" > /etc/php/7.4/mods-available/xdebug.ini
 
 phpenmod -v 7.4 -s cli amqp sqlsrv pdo_sqlsrv redis xdebug imagick
-# Disable : imagick
 
 #useradd -s /bin/bash --home /sources --no-create-home phpuser
 
@@ -64,4 +61,4 @@ groupadd -g ${gid} phpuser
 useradd -l -u ${uid} -g ${gid} -m -s /bin/bash phpuser
 usermod -a -G www-data phpuser
 
-apt-get remove -y libmagickwand-dev libmagickcore-dev && apt autoremove -y
+apt-get remove -y libgcc-8-dev php7.4-dev libmagickwand-dev libmagickcore-dev && apt-get autoremove -y
